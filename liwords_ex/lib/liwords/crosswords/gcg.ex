4f359715446d4c8fc67751df3bd5723b905cbd05
Cesar Del Solar <delsolar@gmail.com>
2017-12-15 23:39:03 -0800

    Progress on gcg parser.

--- liwords_ex/lib/liwords/crosswords/gcg.ex
+++ liwords_ex/lib/liwords/crosswords/gcg.ex
@@ -9,7 +9,47 @@ defmodule LIWords.Crosswords.GCG do
     Takes in a stream and parses it line by line.
   """
   def parse_stream(stream) do
-    Enum.map(stream, fn x -> IO.puts x end)
+    game_repr = %{}
+
+    # Use a keyword list.
+    patterns = [
+      # #player2 cesar cesar
+      player: ~r/#player(?<p_number>[1-2])\s+(?<nick>\S+)\s+(?<real_name>.+)/,
+      # >leesa: GNRUW 8D WRUNG +26 26
+      move: ~r/>(?<nick>\S+):\s+(?<rack>\S+)\s+(?<pos>\w+)\s+(?<play>[\w\\.]+)\s+\+(?<score>\d+)\s+(?<cumul>\d+)/,
+      # `lost_challenge_regex` is taking back of a play after losing a
+      # challenge on it
+      # >leesa: ?CIOSTU --  -61 112
+      lost_challenge: ~r/>(?<nick>\S+):\s+(?<rack>\S+)\s+--\s+-(?<lost_score>\d+)\s+(?<cumul>\d+)/,
+      # Pass can also be used for a lost challenge the other way, that is,
+      # challenging a valid play.
+      # >leesa: ?CIOSTU -  +0 112
+      pass: ~r/>(?<nick>\S+):\s+(?<rack>\S+)\s+-\s+\+0\s+(?<cumul>\d+)/,
+      # Challenge bonus
+      # >Joel: DROWNUG (challenge) +5 289
+      challenge_bonus: ~r/>(?<nick>\S+):\s+(?<rack>\S+)\s+\(challenge\)\s+\+(?<bonus>\d+)\s+(?<cumul>\d+)/,
+      # >howard: III -III +0 299
+      exchange: ~r/>(?<nick>\S+):\s+(?<rack>\S+)\s+-(?<exchanged>\S+)\s+\+0\s+(?<cumul>\d+)/,
+      # >howard:  (Q) +20 411
+      end_rack_points: ~r/>(?<nick>\S+):\s+\((?<rack>\S+)\)\s+\+(?<score>\d+)\s+(?<cumul>\d+)/,
+      # #note aww i should find tarnished :(
+      note: ~r/#note (?<note>.+)/
+    ]
+
+    parser = fn line ->
+      Enum.find_value(patterns,
+        fn {key, pattern} ->
+          with captures when is_map(captures) <- Regex.named_captures(pattern, line) do
+            # notes can be multiline, but everything else should fit on one
+            # line. Use a simple state machine for multiline notes.
+
+            {key, captures}
+          end
+        end
+      )
+    end
+
+    Enum.map(stream, parser)
   end
 
   @doc """
