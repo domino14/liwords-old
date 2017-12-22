b8964e6f4f70492644a6b2e65a550759d246f8b8
Cesar Del Solar <delsolar@gmail.com>
2017-12-22 14:10:15 -0800

    More refactor.

--- static/js/game_summary.jsx
+++ static/js/game_summary.jsx
@@ -2,52 +2,67 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 
-class GameSummary extends React.Component {
-  renderTurns() {
-    const tableRows = [];
-    const player1 = this.props.player1.nick;
-    const player2 = this.props.player2.nick;
+const TurnsTable = (props) => {
+  const tableRows = [];
+  const player1 = props.player1.nick;
+  const player2 = props.player2.nick;
 
-    if (!this.props.turns[player1]) {
-      return null;
-    }
-
-    function transformTurn(turn) {
-      const challenged = turn.challengedOff ? '[Challenged Off]' : '';
-      return `${turn.pos} ${turn.summary} ${challenged} ${turn.score}/${turn.cumul}`;
-    }
-
-    for (let i = 0; i < this.props.turns[player1].length; i += 1) {
-      const row = (
-        <tr key={`rowturn${i}`}>
-          <td>{transformTurn(this.props.turns[player1][i])}</td>
-          {/* Only render second column if it exists */}
-          <td>{this.props.turns[player2] && this.props.turns[player2][i] ?
-            transformTurn(this.props.turns[player2][i]) : ''}
-          </td>
-        </tr>
-      );
-      tableRows.push(row);
-    }
-    return tableRows;
+  if (!props.turns[player1]) {
+    return null;
   }
 
-  render() {
-    return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th>{this.props.player1.nick}</th>
-            <th>{this.props.player2.nick}</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.renderTurns()}
-        </tbody>
-      </table>
+  function transformTurn(turn) {
+    const challenged = turn.challengedOff ? '[Challenged Off]' : '';
+    return `${turn.pos} ${turn.summary} ${challenged} ${turn.score}/${turn.cumul}`;
+  }
+
+  for (let i = 0; i < props.turns[player1].length; i += 1) {
+    const row = (
+      <tr key={`rowturn${i}`}>
+        <td>{transformTurn(props.turns[player1][i])}</td>
+        {/* Only render second column if it exists */}
+        <td>{props.turns[player2] && props.turns[player2][i] ?
+          transformTurn(props.turns[player2][i]) : ''}
+        </td>
+      </tr>
     );
+    tableRows.push(row);
   }
-}
+  return tableRows;
+};
+
+TurnsTable.propTypes = {
+  turns: PropTypes.object.isRequired,
+  player1: PropTypes.shape({
+    real_name: PropTypes.string,
+    p_number: PropTypes.string,
+    nick: PropTypes.string,
+  }).isRequired,
+  player2: PropTypes.shape({
+    real_name: PropTypes.string,
+    p_number: PropTypes.string,
+    nick: PropTypes.string,
+  }).isRequired,
+};
+
+const GameSummary = props => (
+  <table className="table">
+    <thead>
+      <tr>
+        <th>{props.player1.nick}</th>
+        <th>{props.player2.nick}</th>
+      </tr>
+    </thead>
+    <tbody>
+      <TurnsTable
+        player1={props.player1}
+        player2={props.player2}
+        turns={props.turns}
+      />
+    </tbody>
+  </table>
+);
+
 
 GameSummary.propTypes = {
   turns: PropTypes.object.isRequired,
